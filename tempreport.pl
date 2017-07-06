372bb3351b60c4cc54469b10e9112d76d0913ec8
Johannes Ernst <Johannes.Ernst@gmail.com>
2017-07-06 04:16:58 +0000

    Better structure to device hierarchy

--- tempreport.pl
+++ tempreport.pl
@@ -5,12 +5,16 @@ use warnings;
 use POSIX;
 use UBOS::Utils;
 
-my $thermPath = '/sys/bus/w1/devices'; # /28-021501*/w1_slave
 my $thermDev = {
-    '28-021501bb78ff' => 'Attic',
-    '28-021501c3f9ff' => 'Hallway',
-    '28-021501bbc8ff' => 'Office',
-    '28-021501f6c6ff' => 'Server closet'
+    '/sys/class/thermal' => {
+        'thermal_zone0/temp' => 'RPi on-chip'
+    },
+    '/sys/bus/w1/devices' => {
+        '28-021501bb78ff/w1_slave' => 'Attic',
+        '28-021501c3f9ff/w1_slave' => 'Hallway',
+        '28-021501bbc8ff/w1_slave' => 'Office',
+        '28-021501f6c6ff/w1_slave' => 'Server closet'
+    }
 };
 my $gpioDev = {
     '21' => 'Server closet fan'
@@ -19,15 +23,21 @@ my $gpioDev = {
 my( $second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings ) = gmtime( time());
 printf "%15s: %04d-%02d-%02d %02d:%02d:%02d\n", 'Current time', 1900+$yearOffset, $month+1, $dayOfMonth, $hour, $minute, $second;
 
-foreach my $dev ( sort keys %$thermDev ) {
-    my $desc    = $thermDev->{$dev};
-    my $content = UBOS::Utils::slurpFile( "$thermPath/$dev/w1_slave" );
-    my $temp    = '?';
-    if( $content =~ m!t=(\d+)! ) {
-        $temp = $1;
-    }
+foreach my $thermPath ( sort keys %$thermDev ) {
+    foreach my $dev ( sort keys %{$thermDev->{$thermPath}} ) {
+        my $desc    = $thermDev->{$thermPath}->{$dev};
+        my $content = UBOS::Utils::slurpFile( "$thermPath/$dev" );
+        my $temp    = '?';
+        if( $content =~ m!t=(\d+)! ) {
+            # W1 devices
+            $temp = $1;
+        } else {
+            # on-chip
+            $temp = $content;
+        }
 
-    printf "%15s: %d.%1d\n", $desc, floor( $temp / 1000 ), floor( ( $temp % 1000 )/100 + 0.5 );
+        printf "%15s: %d.%1d\n", $desc, floor( $temp / 1000 ), floor( ( $temp % 1000 )/100 + 0.5 );
+    }
 }
 foreach my $dev ( sort keys %$gpioDev ) {
     my $desc = $gpioDev->{$dev};
